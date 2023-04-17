b482ebd285d844a7a8eb79536fd3e036b5fe8948
unknown <thanhdoduc1212@gmail.com>
2023-04-17 17:14:43 +0700

    my changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='trello-clone-web'>
+      <nav className='navbar app'>App bar</nav>
+      <nav className='navbar board'>board bar</nav>
+      <div className='board-columns'>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://lienquan.garena.vn/files/skin/8e4fc278afc09a6304145b7a3f3cf33c5d8c8a82cec6b.jpg" alt="abc"></img>
+            Title: ThanhDo
+          </li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://lienquan.garena.vn/files/skin/8e4fc278afc09a6304145b7a3f3cf33c5d8c8a82cec6b.jpg" alt="abc"></img>
+            Title: ThanhDo
+          </li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://lienquan.garena.vn/files/skin/8e4fc278afc09a6304145b7a3f3cf33c5d8c8a82cec6b.jpg" alt="abc"></img>
+            Title: ThanhDo
+          </li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://lienquan.garena.vn/files/skin/8e4fc278afc09a6304145b7a3f3cf33c5d8c8a82cec6b.jpg" alt="abc"></img>
+            Title: ThanhDo
+          </li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://lienquan.garena.vn/files/skin/8e4fc278afc09a6304145b7a3f3cf33c5d8c8a82cec6b.jpg" alt="abc"></img>
+            Title: ThanhDo
+          </li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://lienquan.garena.vn/files/skin/8e4fc278afc09a6304145b7a3f3cf33c5d8c8a82cec6b.jpg" alt="abc"></img>
+            Title: ThanhDo
+          </li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://lienquan.garena.vn/files/skin/8e4fc278afc09a6304145b7a3f3cf33c5d8c8a82cec6b.jpg" alt="abc"></img>
+            Title: ThanhDo
+          </li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+          <li>add what you like to work on below</li>
+        </ul>
+        <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
-  );
+    
+  )
 }
 
-export default App;
+export default App
